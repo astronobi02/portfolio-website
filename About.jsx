94c5bb2b6db3bc5828878accfa2c3e087a2f2fb3
astronobi02 <pokharelsusan16@gmail.com>
2026-06-08 12:56:54 +0545

    Update About.jsx

--- About.jsx
+++ About.jsx
@@ -1,5 +1,5 @@
 import { motion } from 'framer-motion'
-import { useInView } from '../../hooks'
+import { something } from './hooks'
 import { personalInfo, skillCategories, techStack } from './portfolio'
 import {
   SiReact, SiTypescript, SiNextdotjs, SiNodedotjs, SiPython,
